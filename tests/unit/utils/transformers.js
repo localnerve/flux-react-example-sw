4028839c0dd88d106eb5b34d31f4ad0d128d8561
localnerve <alex@localnerve.com>
2015-02-16 17:49:13 -0500

    added transformers unit test

---
+++ tests/unit/utils/transformers.js
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2015 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
+ */
+/* global describe, it, beforeEach */
+'use strict';
+
+var expect = require('chai').expect;
+
+var routesResponse = require('../../fixtures/routes-response');
+var transformers = require('../../../utils/transformers');
+var MockContext = require('fluxible/utils/MockActionContext')();
+// var EventEmitter = require('events').EventEmitter;
+// var MockStore = require('fluxible/utils/createStore');
+
+describe('transformers', function () {
+  describe('toFluxibleRoutes', function() {
+    var jsonRoutes;
+    var context;
+    var toFluxibleRoutes = transformers.toFluxibleRoutes;
+/*
+    var STORE_EVENT = 'storeEvent';
+    var makeStore = function(emitter) {
+      return new MockStore({
+        storeName: 'MockStore',
+        handlers: {
+          'CHANGE_ROUTE_SUCCESS': 'bang',
+          'UPDATE_PAGE_TITLE': 'bang',
+          'RECEIVE_ROUTES': 'bang'
+        },
+        bang: function() {
+          emitter.emit(STORE_EVENT, arguments);
+        }
+      });
+    };
+*/
+
+    beforeEach(function() {
+      // clone routesResponse so we don't disrupt routes-response.
+      jsonRoutes = JSON.parse(JSON.stringify(routesResponse));
+      context = new MockContext();
+    });
+
+    it('should transform json routes to fluxible routes', function(done) {    
+      var testKey = 'home';
+
+      // check input types
+      expect(jsonRoutes).to.be.an('object');
+      expect(jsonRoutes[testKey].action).to.be.an('object');
+
+      var fluxibleRoutes = toFluxibleRoutes(jsonRoutes);
+
+      // check output types
+      expect(fluxibleRoutes).to.be.an('object');
+      expect(fluxibleRoutes[testKey].action).to.be.a('function');
+
+      // check that no keys are lost
+      expect(Object.keys(jsonRoutes).length).to.equal(
+        Object.keys(fluxibleRoutes).length
+      );
+
+      // check that the output action is workable
+      fluxibleRoutes[testKey].action(context, {}, done);
+    });
+
+    it('should throw if an unknown action is specified', function() {
+      jsonRoutes.home.action.name = 'unknown_action';
+
+      expect(function() {
+        toFluxibleRoutes(jsonRoutes);
+      }).to.throw(Error, /not found/);
+    });
+  });
+});

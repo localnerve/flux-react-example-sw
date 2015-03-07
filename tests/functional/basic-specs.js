6f83ce627f9b505b9b9fe7447be625e0c6ecf648
localnerve <alex@localnerve.com>
2015-03-07 01:38:19 -0500

    cleanup functional tests

---
+++ tests/functional/basic-specs.js
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2015 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
+ */
+ /* global it */
+'use strict';
+
+var test = require('./sauce-travis');
+
+it('should get home page and navigate to about', function(done) {
+  test.state.browser
+    .get(test.baseUrl)
+    .title()
+    .should.eventually.include('Flux React Example')
+    .elementByTagName('h1')
+    .text()
+    .should.eventually.include('Hello World')
+    .elementByLinkText('About')
+    .click()
+    .waitForElementByTagName('h1')
+    .text()
+    .should.eventually.include('Example About Page')
+    .nodeify(done);
+});
+
+it('should get about page and navigate to home', function(done) {
+  test.state.browser
+    .get(test.baseUrl+'/about')
+    .title()
+    .should.eventually.include('About')
+    .elementByTagName('h1')
+    .text()
+    .should.eventually.include('About')
+    .elementByLinkText('Home')
+    .click()
+    .waitForElementByTagName('h1')
+    .text()
+    .should.eventually.include('Hello World')
+    .nodeify(done);
+});

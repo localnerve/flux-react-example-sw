37c0c7d4a5ab2172bcc5159a15e57b0627588136
localnerve <alex@localnerve.com>
2015-04-07 10:34:20 -0400

    update application component test to use dynamic content

--- tests/unit/components/Application.js
+++ tests/unit/components/Application.js
@@ -13,17 +13,27 @@ var testDom = require('../../utils/testdom');
 // This is an isomorphic component, so for testing, prepare to load react with dom here
 testDom();
 
-var ApplicationStore = require('../../../stores/ApplicationStore');
 var createMockComponentContext = require('fluxible/utils').createMockComponentContext;
+
+var ApplicationStore = require('../../../stores/ApplicationStore');
+var ContentStore = require('../../../stores/ContentStore');
+var serviceData = require('../../fixtures/service-data');
+var routesResponse = require('../../fixtures/routes-response');
+var fluxibleRoutes = require('../../fixtures/fluxible-routes');
 var fluxibleApp = require('../../../app');
-var React = require('react/addons');
+
+var React = require('react');
+var ReactAddons = require('react/addons');
 
 describe('application component', function() {
-  var AppFactory, appElement, context, testUtils;
+  var appElement, context, testUtils;
 
   var homeRoute = {
     params: { key: 'home' },
-    config: { page: 'home' }
+    config: routesResponse.home
+  };
+  var homePage = {
+    resource: routesResponse.home.action.params.resource
   };
 
   function makeHomePath() {
@@ -31,38 +41,46 @@ describe('application component', function() {
   }
 
   before(function() {
-    AppFactory = fluxibleApp.getComponent();
-    testUtils = React.addons.TestUtils;
+    testUtils = ReactAddons.addons.TestUtils;
+
+    serviceData.fetch(homePage, function(err, content) {
+      if (err) {
+        throw err;
+      }
+      homePage.content = content;
+    });
   });
 
   beforeEach(function() {
     context = createMockComponentContext({
-      stores: [ApplicationStore]
+      stores: [ApplicationStore, ContentStore]
     });
     context.makePath = makeHomePath;
-    appElement = AppFactory({
+
+    var appStore = context.getStore(ApplicationStore);
+    var contentStore = context.getStore(ContentStore);
+
+    appStore.receiveRoutes(fluxibleRoutes);
+    appStore.handleNavigate(homeRoute);
+    contentStore.receivePageContent(homePage);
+
+    appElement = React.createElement(fluxibleApp.getComponent(), {
       context: context
-    });    
+    });
+
     testDom();
   });
 
-  it.skip('should render hello world heading', function() {
-    var appStore = context.getStore(ApplicationStore);
+  it('should render home content', function() {
     var app = testUtils.renderIntoDocument(appElement);
-    
-    // update state
-    appStore.handleNavigate(homeRoute);
 
-    var component = testUtils.findRenderedDOMComponentWithTag(app, 'h2');
-    expect(component.getDOMNode().textContent).to.match(/hello world/i);
+    var component = testUtils.findRenderedDOMComponentWithClass(app, 'page-content');
+
+    expect(component.getDOMNode().textContent).to.match(/Home/i);
   });
 
-  it.skip('should render navigation', function() {
-    var appStore = context.getStore(ApplicationStore);
+  it('should render navigation', function() {
     var app = testUtils.renderIntoDocument(appElement);
-    
-    // update state
-    appStore.handleNavigate(homeRoute);
 
     // throws if not exactly 1
     testUtils.findRenderedDOMComponentWithTag(app, 'ul');

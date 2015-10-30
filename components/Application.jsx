c718144570a22943c48a902592af57ab0c2cb7d2
Alex Grant <alex@localnerve.com>
2015-10-29 23:29:53 -0400

    adjust modal colors

--- components/Application.jsx
+++ components/Application.jsx
@@ -74,13 +74,17 @@ var Application = React.createClass({
           isOpen={this.props.modal.open}
           onRequestClose={this.modalClose}
           style={{
+            overlay: {
+              backgroundColor: 'rgba(70, 70, 70, 0.75)'
+            },
             content: {
               top: '50%',
               left: '50%',
               right: 'auto',
               bottom: 'auto',
               marginRight: '-50%',
-              transform: 'translate(-50%, -50%)'
+              transform: 'translate(-50%, -50%)',
+              background: 'rgba(255, 255, 255, 0.93)'
             }
           }}
           >

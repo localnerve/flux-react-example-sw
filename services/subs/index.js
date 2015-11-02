15e986641f76527a0005336a961b4f39b08377cf
Alex Grant <alex@localnerve.com>
2015-11-01 23:56:34 -0500

    update subscription

--- services/subs/index.js
+++ services/subs/index.js
@@ -4,10 +4,13 @@
  */
 'use strict';
 
+var debug = require('debug')('Example:Subscriptions');
 var data = require('../data');
 
 // FIXME:
 // Subscription storage should not be in this process in a real application.
+// This is particularly fatal for subs, because browser sub state will be out
+// of sync with the server.
 var subscriptions = {};
 
 /**
@@ -20,16 +23,20 @@ var subscriptions = {};
  */
 function create (subscriptionId, endpoint, callback) {
   if (!subscriptionId || !endpoint) {
+    debug('create: Bad subscriptionId or endpoint supplied');
     return callback(new Error ('Bad subscriptionId or endpoint supplied'));
   }
 
   if (subscriptions[subscriptionId]) {
+    debug('create: subscriptionId already exists');
     return callback(new Error('subscriptionId already exists'));
   }
 
   data.fetch({
     resource: 'settings'
   }, function (err, data) {
+    debug('create: fetch settings: ', err, data);
+
     if (err) {
       return callback(err);
     }
@@ -50,8 +57,8 @@ function create (subscriptionId, endpoint, callback) {
 }
 
 /**
- * Read push notificaiton topics for a subscription.
- * If no subscription id supplied, return all available topics.
+ * Read push notification topics for a subscription.
+ * If no subscription id supplied, or subscription not found, return all available topics.
  *
  * @param {String} [subscriptionId] - The user's subscription ID.
  * @param {Function} callback - Called on completion.
@@ -60,21 +67,22 @@ function read (subscriptionId, callback) {
   if (subscriptionId) {
     var subscription =  subscriptions[subscriptionId];
 
-    if (!subscription) {
-      return callback(new Error('No subscription found for '+subscriptionId));
+    if (subscription) {
+      debug('read: subscription topics found', subscriptions[subscriptionId].topics);
+      return callback(null, subscriptions[subscriptionId].topics);
     }
-
-    return callback(null, subscriptions[subscriptionId].topics);
   }
 
   data.fetch({
     resource: 'settings'
   }, function (err, data) {
+    debug('read: fetch settings: ', err, data);
+
     if (err) {
       return callback(err);
     }
 
-    return callback(null, data.pushNotifications.topics);
+    return callback(null, data.content.pushNotifications.topics);
   });
 }
 
@@ -90,10 +98,12 @@ function update (subscriptionId, updateTopics, endpoint, callback) {
   var subscription = subscriptions[subscriptionId];
 
   if (!subscription) {
+    debug('update: No subscription found for '+subscriptionId);
     return callback(new Error('No subscription found for '+subscriptionId));
   }
 
   subscription.endpoint = endpoint || subscription.endpoint;
+  debug('update: updated endpoint = ' + !!endpoint);
 
   if (updateTopics) {
     subscription.topics.forEach(function (subscribedTopic) {
@@ -102,6 +112,8 @@ function update (subscriptionId, updateTopics, endpoint, callback) {
       });
 
       if (updates && updates.length > 0) {
+        debug('update: '+subscribedTopic.label+' updated, subscribe = '+updates[0].subscribed);
+
         // first update wins.
         subscribedTopic.subscribed = updates[0].subscribed;
       }
@@ -119,11 +131,13 @@ function update (subscriptionId, updateTopics, endpoint, callback) {
  */
 function unsubscribe (subscriptionId, callback) {
   if (!subscriptions[subscriptionId]) {
+    debug('unsubscribe: No subscription found for '+subscriptionId);
     return callback(new Error('No subscription found for '+subscriptionId));
   }
 
   delete subscriptions[subscriptionId];
 
+  debug('unsubscribe: deleted subscription '+subscriptionId);
   return callback();
 }
 

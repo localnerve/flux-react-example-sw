387c9bb6d752c9636b1d71372bc7804ef6bbf332
Alex Grant <alex@localnerve.com>
2016-01-06 18:22:15 -0500

    add json method

--- tests/mocks/request.js
+++ tests/mocks/request.js
@@ -2,6 +2,7 @@
  * Copyright 2015, Alex Grant, LocalNerve, LLC.
  * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
  */
+/* global Promise */
 'use strict';
 
 function Request (url, options) {
@@ -10,5 +11,9 @@ function Request (url, options) {
   this.body = options.body;
   this.credentials = options.credentials;
 }
+
+Request.prototype.json = function () {
+  return Promise.resolve(this.body);
+};
 
 module.exports = Request;

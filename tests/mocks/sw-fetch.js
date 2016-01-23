f6c16046bc9fa015bab48983c396a80d100f550e
Alex Grant <alex@localnerve.com>
2016-01-23 17:52:53 -0500

    add global fetch, first pass

---
+++ tests/mocks/sw-fetch.js
@@ -0,0 +1,35 @@
+/***
+ * Copyright (c) 2015 Alex Grant (@localnerve), LocalNerve LLC
+ * Copyrights licensed under the BSD License. See the accompanying LICENSE file for terms.
+ *
+ * A simple mock for service worker exposed global fetch method.
+ */
+/* global Promise */
+'use strict';
+
+var Response = require('./response');
+
+var mockResponse;
+var emulateError;
+
+function fetch (request) {
+  if (emulateError) {
+    return Promise.reject(new Error('mock error'));
+  }
+
+  return Promise.resolve(mockResponse || new Response({
+    mock: 'body'
+  }, {
+    status: 200
+  }));
+}
+
+module.exports = {
+  fetch: fetch,
+  setMockResponse: function (response) {
+    mockResponse = response;
+  },
+  setEmulateError: function (err) {
+    emulateError = err;
+  }
+};
